0197c029709768213249c28ffb74c8a0d7311439
Amelia Kuang <qianyi.kuang@gmail.com>
2020-06-21 09:40:09 -0700

    updated style

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,5 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-json`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
