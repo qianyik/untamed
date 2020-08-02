8cb5f7d85cbefca33415925f46d21d0287f618da
Amelia Kuang <qianyi.kuang@gmail.com>
2020-08-02 10:53:19 -0700

    Added top navigation

--- src/templates/novel.js
+++ src/templates/novel.js
@@ -4,30 +4,8 @@ import { Helmet } from "react-helmet"
 
 import "./novel.css"
 
-const Novel = ({ pageContext }) => (
-  <div className="page">
-    <Helmet>
-      <title>{pageContext.title}</title>
-      <meta name="robots" content="noindex, nofollow" />
-      <link
-        rel="icon"
-        href="https://res.cloudinary.com/kamalia/image/upload/v1559861791/logo_kxcn4i.png"
-      ></link>
-    </Helmet>
-
-    <h1 className="title">{pageContext.title}</h1>
-
-    {/* <div className="novel-link-top">
-      <Link to="/catalog" className="novel-link-item">
-        Catalog
-      </Link>
-    </div> */}
-
-    <div
-      className="content"
-      dangerouslySetInnerHTML={{ __html: pageContext.content }}
-    />
-
+const Novel = ({ pageContext }) => {
+  const navigator = (
     <div className="novel-link">
       {pageContext.id > 1 && (
         <Link
@@ -49,7 +27,31 @@ const Novel = ({ pageContext }) => (
         </Link>
       )}
     </div>
-  </div>
-)
+  )
+
+  return (
+    <div className="page">
+      <Helmet>
+        <title>{pageContext.title}</title>
+        <meta name="robots" content="noindex, nofollow" />
+        <link
+          rel="icon"
+          href="https://res.cloudinary.com/kamalia/image/upload/v1559861791/logo_kxcn4i.png"
+        ></link>
+      </Helmet>
+
+      <h1 className="title">{pageContext.title}</h1>
+
+      {navigator}
+
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{ __html: pageContext.content }}
+      />
+
+      {navigator}
+    </div>
+  )
+}
 
 export default Novel
